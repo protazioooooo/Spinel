3a37eeb4dbd0e83b89860c5ea5ece631e8eba2f9
protazioooooo <protaziomatheusrip>
2022-08-06 09:13:14 -0300

    Test

--- Main.lua
+++ Main.lua
@@ -1,5 +1,2 @@
 local Library = game:HttpGet('https://raw.githubusercontent.com/protazioooooo/Spinela/main/Library.lua', true)
-
-repeat
-    task.wait()
-until game:IsLoaded() == true
+Library.Initialization()
