78f933a96422e3007dcc683efc157db9c8afa3f7
protazioooooo <protaziomatheusrip>
2022-08-09 20:35:54 -0300

    Test

--- Main.lua
+++ Main.lua
@@ -1,1 +1,9 @@
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/protazioooooo/Spinel/main/Library.lua'), true)()
+local Main = Library:CreateMain()
+
+Main:CreateAimAssist()
+Main:CreateUtilities()
+Main:CreateTeleports()
+Main:CreateTeleports()
+Main:CreateRenderizations()
+Main:CreatePowerfulls()
